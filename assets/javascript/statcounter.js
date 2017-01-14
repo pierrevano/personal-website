5a47afe984b4e9210418188739ef6ba11d9ea9bd
Pierre Vanoverschelde <pierre.vano@gmail.com>
2017-01-14 16:15:37 +0100

    editing statcounter js file

--- assets/javascript/statcounter.js
+++
@@ -1,8 +0,0 @@
-var sc_project=9076218;
-var sc_invisible=1;
-var sc_security="c10d6c16";
-var scJsHost = (("https:" == document.location.protocol) ?
-"https://secure." : "http://www.");
-document.write("<sc"+"ript type='text/javascript' src='" +
-scJsHost+
-"statcounter.com/counter/counter.js'></"+"script>");
